9bd510bfa18a339a352bef02515fab68e0fd68a8
flodardev <63950579+flodardev@users.noreply.github.com>
2020-07-20 17:11:08 +0800

    complete project

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { motion } from "framer-motion";
 
-const Order = ({ pizza }) => {
-  return (
-    <div className="container order">
-      <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+const containerVariants = {
+	hidden: {
+		opacity: 0,
+		x: "100vw",
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			type: "spring",
+			mass: 0.4,
+			damping: 8,
+			when: "beforeChildren",
+			staggerChildren: 0.2,
+		},
+	},
+	exit: {
+		x: "-100vw",
+		transition: {
+			ease: "easeInOut",
+		},
+	},
+};
+
+const childVariants = {
+	hidden: {
+		opacity: 0,
+	},
+	visible: {
+		opacity: 1,
+	},
+};
+
+const Order = ({ pizza, setShowModal }) => {
+	useEffect(() => {
+		setTimeout(() => {
+			setShowModal(true);
+		}, 5000);
+	}, [setShowModal]);
+
+	return (
+		<motion.div
+			className="container order"
+			variants={containerVariants}
+			initial="hidden"
+			animate="visible"
+			exit="exit"
+		>
+			<h2>Thank you for your order :)</h2>
+			<motion.p variants={childVariants}>
+				You ordered a {pizza.base} pizza with:
+			</motion.p>
+			{pizza.toppings.map((topping) => (
+				<motion.div key={topping} variants={childVariants}>
+					{topping}
+				</motion.div>
+			))}
+		</motion.div>
+	);
+};
 
 export default Order;
